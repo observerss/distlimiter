8ae29f404926159c4596332f16b38f3ad2913d23
hujingchao <hujingchao@laiye.com>
2019-04-29 20:25:29 +0800

    add install requires

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     version='0.1.1',
     description='Distributed Limiter based on Redis',
     author='Jingchao Hu',
+    install_requires=['hiredis', 'redis'],
     author_email='jingchaohu@gmail.com',
     url='https://github.com/observerss/distlimiter',
     packages=['distlimiter'],
